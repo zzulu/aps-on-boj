725ae3d9ba6945da0b35549e8496c2ddcd682394
Junwoo Hwang <zzulu.hwang@gmail.com>
2020-08-20 10:48:09 +0900

    Clean up

--- solutions/py/2448.py
+++ solutions/py/2448.py
@@ -5,4 +5,5 @@ def stars(n, lindent=3, rindent=3):
     else:
         return stars(n//2, lindent*2, rindent*2) + [x+' '+y for x, y in zip(stars(n//2, lindent*2-3), stars(n//2, rindent=rindent*2-3))]
 
-print('\n'.join(stars(24)))
+
+print('\n'.join(stars(int(input()))))
